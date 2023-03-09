48ce40b7b30edc5be9e6383eda22d31a0da6a0aa
Nontobeko Mhlongo <ntobsie2010@gmail.com>
2023-03-09 16:33:06 +0200

    API

---
+++ API/config/index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+let { createPool } = require("mysql");
+
+let connection = createPool({
+  host: process.env.HOST,
+  db: process.env.DB_NAME,
+  user: process.env.USERNAME,
+  password: process.env.DB_PWD,
+  port: process.env.DB_PORT,
+  multipleStatements: true,
+});
+module.exports = connection;
